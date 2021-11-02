e18900aa9e97cc1d9a5bd13d22179e6c24f88c54
zeenat08 <zeenatkhanbfs@gmail.com>
2021-11-02 21:05:45 +0530

    Welcome to Address Book Problems

---
+++ AddressBook_Collection_Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("\n************\nWELCOME TO ADDRESS BOOK PROGRAM\n*************\n\n");
+        }
+    }
+}
